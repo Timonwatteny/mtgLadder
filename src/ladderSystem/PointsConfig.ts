57aad6a3187a62e16f3acdb25ebcf6405fcbe861
LouisBoucquet <louisboucquet@gmail.com>
2019-10-22 14:57:48 +0200

    made PointsConifg class

---
+++ src/ladderSystem/PointsConfig.ts
@@ -0,0 +1,21 @@
+class PointConfig {
+	constructor(
+		private _win: number,
+		private _lose: number,
+		private _draw: number
+	) {
+	}
+
+	public get win(): number {
+		return this._win;
+	}
+
+	public get lose(): number {
+		return this._lose;
+	}
+
+	public get draw(): number {
+		return this._draw;
+	}
+
+}
